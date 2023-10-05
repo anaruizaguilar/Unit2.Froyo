d94e09ab494545f4225dc4bbf4ab1976a07c4a87
Ana Ruiz <aruiz_90@hotmail.com>
2023-10-05 11:03:06 -0500

    final update

--- froyo.js
+++ froyo.js
@@ -13,30 +13,24 @@ const stringArray = userInputString.split(",");
     // }; 
     // console.log(stringArray);
 
-    //container for order
-const iceCreamOrder = {
-    vanilla: 0,
-    chocolate: 0,
-    strawberry: 0,
-    coffee: 0
-};
+    //Initializing container for order without assigning keys:
+const iceCreamOrder = {};
 
 function printIceCreamOrder(stringArray) {
     // "Cannot read properties of undefined (reading length)" error here:
-    for(i = 0; i < stringArray.length; i++) {
-    //access the individual flavors
-    //increase the count of ice cream order depending on repetition of flavor
-    //adding 1 to flavor count 
-    for(const key in iceCreamOrder) {
-        if(stringArray[i] === "vanilla") {
-            iceCreamOrder["vanilla"] = iceCreamOrder["vanilla"] + 1;
-        };
-    };
-    // //once loop is complete, console.table order 
-    //     console.table(iceCreamOrder);
+    for(let i = 0; i < stringArray.length; i++) {
+    //^^this loop will iterate through the array passed by the user.
+    //the conditional statements below will use the flavors in the array as keys in the iceCreamOrder object
+    //if the flavor is in the array more than once, the conditional statment will increment its count, otherwise it will assign the value as 1  
+        if(iceCreamOrder[stringArray[i]]) {
+            iceCreamOrder[stringArray[i]]++;
+        } else {
+            iceCreamOrder[stringArray[i]] = 1;
+        }
 };
     //once loop is complete, console.table order 
+    console.log("Your order is as follows:");
     console.table(iceCreamOrder);
 };
-//fixed "undefined" error on line 26 by passing "stringArray" as a parameter to the function:
+//fixed "undefined" error on line 20 by passing "stringArray" as a parameter to the function:
 printIceCreamOrder(stringArray);
